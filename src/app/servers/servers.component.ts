78614bcbcf47f18bf6b2aa06066c4dca1d6fa2f5
hiredtocode <jasonhan@kakao.com>
2021-03-27 22:04:10 +0900

    *ngIf - display DOM element conditionally

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
-  userName = '';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -19,6 +19,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus =
       'Server was created! Name is ' + this.serverName;
   }
